8c9968f16b5be7c4145ef21ca861209755a5c53e
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2016-07-20 16:44:26 +0300

    .

---
+++ mysite/webblog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+    yazar =models.ForeignKey('auth.User')
+    baslik=models.CharField(max_length=200)
+    yazi=models.TextField()
+    olusturma_tarihi=models.DateTimeField(default=timezone.now)
+    yayinlama_tarihi=models.DateTimeField(blank=True,null=True)
+
+    def yayinla(self):
+        self. yayinlama_tarihi=timezone.now
+        self.save()
+    def __str__(self):
+    	return self.baslik
